ff6a1ea799263bf29b9e332255033f45f2642f0a
Jose-V12 <jv123144@gmail.com>
2022-12-15 09:09:02 -0600

    inserted coding into index.js File to be linked to mongodb

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
+//Middleware
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+//define port
+const PORT = 3002
+const router = require("./routes/patient-routes");
+//localhost:3002/patient
+app.use("/patient", router)
+//establish link to mongoDB Atlas Database
+mongoose.connect("mongodb+srv://Jose-V:Josevargas123144!@cluster0.2qienb5.mongodb.net/Hospital?retryWrites=true&w=majority"
+).then(() => console.log("Connected To Database"))
+.then(()=>console.log("Backend Server Listening on PORT " + PORT))
+.then(() =>{
+    app.listen(PORT)
+}).catch((err) => console.log(err));
