9d12a7635a9e56c7f5dc38233ead73d62fed0ccd
Jose-V12 <jv123144@gmail.com>
2022-12-15 09:14:31 -0600

    fixed error in index.js to successfully link to mongodb

--- server/index.js
+++ server/index.js
@@ -2,17 +2,15 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
+
 //Middleware
 app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 //define port
 const PORT = 3002
-const router = require("./routes/patient-routes");
-//localhost:3002/patient
-app.use("/patient", router)
 //establish link to mongoDB Atlas Database
-mongoose.connect("mongodb+srv://Jose-V:Josevargas123144!@cluster0.2qienb5.mongodb.net/Hospital?retryWrites=true&w=majority"
+mongoose.connect("mongodb+srv://Jose-V:Josevargas123144!@cluster0.0pptu4i.mongodb.net/Hospital?retryWrites=true&w=majority"
 ).then(() => console.log("Connected To Database"))
 .then(()=>console.log("Backend Server Listening on PORT " + PORT))
 .then(() =>{
