11d83548c2107bdfbe86e1ae2d466717639a8de6
Jose-V12 <jv123144@gmail.com>
2022-12-15 09:20:47 -0600

    Created routes for the controllers

---
+++ server/Routes/patient-routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const patientControllers = require("../Controllers/patient-controllers");
+//assign functionality to requests for URLs
+//localhost:3001/patient/
+router.get("/", patientControllers.getAllPatients);
+//localhost:3001/patient/
+router.post("/", patientControllers.addPatient);
+//localhost/3001/patient/:id
+router.get("/:id", patientControllers.getById);
+//localhost:3001/patient/:id
+router.put("/:id", patientControllers.updatePatient);
+//localhost:3001/patient/:id
+router.delete("/:id", patientControllers.deletePatient);
+module.exports = router;
